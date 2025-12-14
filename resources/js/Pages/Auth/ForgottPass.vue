6fbd485d9efcee5af16ff9225838d7eb4b2acf7b
Timothyminani <minanitimothy@gmail.com>
2025-12-15 02:17:12 +0300

    Initial commit: deploy Laravel project with Vite build

---
+++ resources/js/Pages/Auth/ForgottPass.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Your form or content here -->
+    <h1>Forgot Password Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ForgottPass",
+  // optional data, methods, etc.
+};
+</script>
+
+<style scoped>
+/* optional CSS */
+</style>
